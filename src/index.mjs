6b6703619b2e09e55bcf6707f602b1696d51c582
LiamJly <joly.liam@gmail.com>
2025-07-25 20:57:42 +0200

    Fix Github Actions workflow and update bot to use env variables

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,7 @@
 import { SapphireClient } from '@sapphire/framework';
 import { GatewayIntentBits } from 'discord.js';
-import config from '../config/config.json' assert { type: 'json' };
 
-
-const token = config.token
+const token = process.env.DISCORD_TOKEN
 const client = new SapphireClient({
   intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
   loadMessageCommandListeners: true
